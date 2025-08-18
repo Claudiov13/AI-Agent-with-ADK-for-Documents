bffd9a07437372609cbf2f594eff99b42f7259e7
Claudiov13 <claudiovargas77@gmail.com>
2025-08-18 15:44:59 -0300

    Update agent.py
    Ajustes de descaracterização de projeto

--- Acessa _facil/agent.py
+++ Acessa _facil/agent.py
@@ -2,18 +2,14 @@ from google.adk import Agent
 from google.adk.tools import VertexAiSearchTool
 
 
-search_tool = VertexAiSearchTool(data_store_id="projects/414873609601/locations/global/collections/default_collection/dataStores/acessafacil-search_1755114140363")
+search_tool = VertexAiSearchTool(data_store_id="copyID google cloud APP")
 
 # Cria o agente principal
 root_agent = Agent(
     model="gemini-2.5-pro",
-    name="Acessafácil",
+    name="xxxxxx",
     instruction="""
-    Para qualquer pergunta sempre use a tool search_tool, Você é um assistente especializado em analisar e extrair informações de documentos normativos de Network Processes Management.  
-Responda com base exclusiva em evidências documentais, preservando código e título completo das fontes consultadas.  
-Extraia dados conforme regras específicas para KPI, responsáveis, controles SOX, colaboradores, áreas e atividades Netflow.  
-Declare ausência de informações quando não encontradas e nunca invente, generalize ou misture dados não relacionados.  
-Mantenha linguagem técnica, clara e profissional, garantindo rastreabilidade e coerência em respostas consolidadas.
+   Any instruction as you want
     """,
     tools=[search_tool]
 )
